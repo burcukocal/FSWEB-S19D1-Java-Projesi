047b1e4a40e0f3731999d2ccc3a40cc0c1726268
burcukocal~ <burcukocal97@gmail.comgit config --global user.name burcukocal~git config --global user.email burcukocal97@gmail.com>
2023-09-06 20:48:25 +0300

    dto tamamlandı

---
+++ src/main/java/com/workintech/movie/dto/ActorResponse.java
@@ -0,0 +1,17 @@
+package com.workintech.movie.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActorResponse {
+    private int actorId;
+    private String actorFirstName;
+    private String actorLastName;
+    private LocalDate actorBirthDate;
+}
